7fed5233f902a93bce7078d6dad1d76e53a3f79e
CH <clint@thestaticvoid.net>
2022-08-19 00:17:29 +0200

    Added io/espruino/button

---
+++ io/espruino/button/README.md
@@ -0,0 +1,38 @@
+# espruino / button
+
+Sends data while the button is being pressed.
+
+Read more:
+* [ixfx Espruino module](https://clinth.github.io/ixfx/modules/Io.Espruino.html)
+* [Puck.js button](https://www.espruino.com/Puck.js#button)
+* [Espruino setWatch function](https://www.espruino.com/Reference#l__global_setWatch)
+
+`scripts.trigger` as given as an example for sending data when the button is pressed. It is dynamically sent to the Puck when connected.
+
+Here is a readable, expanded versios so you can see what's going on. It is borrowed [from the Espruino docs](https://www.espruino.com/Puck.js#button).
+
+`scripts.trigger`:
+```js
+// Initialise an interrupt, calling the function
+// whenever the state of `BTN` changes
+setWatch(evt => {
+  // Copy some values
+  const state = { 
+    state: evt.state,
+    time: evt.time,
+    lastTime: evt.lastTime
+  };
+
+  // Make a JSON string
+  const json = JSON.stringify(state);
+
+  // Send it via Bluetooth
+  Bluetooth.println(json);
+}, BTN, {edge:"both", debounce:50, repeat:true});
+
+// If Bluetooth disconnects, reset the board
+// so the code doesn't run any more. This saves battery.
+NRF.on('disconnect', () => reset());
+```
+
+ 
---
+++ io/espruino/button/script.js
@@ -0,0 +1,114 @@
+
+import { delay } from '../../../ixfx/flow.js';
+import { Espruino } from '../../../ixfx/io.js';
+
+const scripts = Object.freeze({
+  // Whenever `BTN` changes, send data
+  trigger: `setWatch((evt) => Bluetooth.println(JSON.stringify({state:evt.state,time:evt.time,lastTime:evt.lastTime})), BTN, {edge:"both", debounce:50, repeat:true});NRF.on('disconnect', () => reset());`
+});
+
+const settings = Object.freeze({
+  script: scripts.trigger
+});
+
+let state = Object.freeze({
+  /** @type number */
+  time: 0,
+  /** @type number */
+  lastTime: 0,
+  /** @type boolean */
+  pressed:false
+});
+
+const useState = () => {
+  const { time, lastTime, pressed } = state;
+  const elapsed = time - (lastTime ?? time);
+  setHtml(`pressed`, `Pressed (${elapsed} elapsed)`);
+  console.log(`time: ${time} last: ${lastTime} pressed: ${pressed} elapsed: ${elapsed}`);
+  setClass(`pressed`, pressed, `on`);
+};
+
+const setHtml = (id, value) => {
+  const el = document.getElementById(id);
+  if (!el) return;
+  el.innerHTML = value;
+};
+
+
+const setClass = (id, value, className) => {
+  const el = document.getElementById(id);
+  if (!el) return;
+  if (value) el.classList.add(className);
+  else el.classList.remove(className);
+};
+
+const setup = () => {
+  const { script } = settings;
+  const onConnected = (connected) => {
+    setCssDisplay(`preamble`,  connected ? `none` : `block`);
+    setCssDisplay(`data`, connected ? `block` : `none`);
+  };
+
+  const connect = async () => {
+    try {
+      // Connect to Puck
+      const p = await Espruino.puck();
+      console.log(`Connected`);
+      const onData = (evt) => {
+        let data = evt.data.trim(); // Remove line breaks etc
+        if (data.startsWith(`>`)) data = data.substring(1); // Trim off starting > if it appears
+        if (!data.startsWith(`{`)) return;
+        if (!data.endsWith(`}`)) return;
+
+        try {
+          const d = JSON.parse(data);
+          updateState({
+            pressed: d.state,
+            time: d.time,
+            lastTime: d.lastTime
+          });
+          useState();
+        } catch (ex) {
+          console.warn(ex);
+          console.log(data);
+        }
+      };
+      // Listen for events
+      p.addEventListener(`change`, evt => {
+        console.log(`${evt.priorState} -> ${evt.newState}`);
+      });
+
+
+      // Send script after a moment
+      delay(async () => {
+        await p.writeScript(script);
+        onConnected(true);
+        p.addEventListener(`data`, onData);
+      }, 1000);
+
+    } catch (ex) {
+      console.error(ex);
+    }
+  };
+
+  document.getElementById(`btnConnect`)?.addEventListener(`click`, connect);
+};
+setup();
+
+
+function setCssDisplay(id, value) {
+  const el = document.getElementById(id);
+  if (!el) return;
+  el.style.display = value;
+}
+
+/**
+ * Update state
+ * @param {Partial<state>} s 
+ */
+function updateState (s) {
+  state = Object.freeze({
+    ...state,
+    ...s
+  });
+}
