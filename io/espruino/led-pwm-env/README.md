299879d23c78b53b1da2437e84fde7769503e02a
CH <clint@thestaticvoid.net>
2022-09-01 12:27:25 +0200

    io/espruino/led-pwm-env

---
+++ io/espruino/led-pwm-env/README.md
@@ -0,0 +1,45 @@
+# espruino / led-pwm-env
+
+Demonstrates toggling LEDs, blinking LEDs and using pulse width modulation to fade LEDs on the [Puck.js](https://www.espruino.com/Puck.js).
+
+Read more:
+* [ixfx Espruino module](https://clinth.github.io/ixfx/modules/Io.Espruino.html)
+* [Puck.js LED](https://www.espruino.com/Puck.js#leds)
+
+
+## Pulse width modulation
+
+[Pulse width modulation](https://www.espruino.com/PWM) toggles a digital pin on and off so fast it can be perceived as being a higher or lower intensity. When used with an LED, this effectively changes the brightness of an LED.
+
+In Espruino, [analogWrite](https://www.espruino.com/Reference#l__global_analogWrite) is way of doing so. The pulse width modulation amount is a value given in 0..1, where 0 is off and 1 is fully on. 0.5 then equates to 50% intensity.
+
+Espruino code:
+```js
+// 50% pulse width modulation
+analogWrite(LED1, 0.5);
+```
+
+In the example sketch, a helper function (`setLedPwm`) is used to send this code to the Espruino.
+
+With LEDs, the perceived brightness does not necessarily correspond linearly to the pulse-width moduation amount. In certain ranges or lighting conditions, changes PWM amount can seem to have no apparent effect.
+
+Because LEDs are so intense, using the silicon cover of the Puck.js, a piece of paper or something translucent is useful for physically 'scaling' the output.
+
+## Things to try
+
+### On the Espruino
+
+* Integrate with the button, allowing each LED to be turned on in turn, and then turning them all off.
+* Control LED brightness (via PWM) according to the gyroscope value
+* Make a blink function that takes a 'speed' parameter.
+
+### With the browser too
+
+* Modulate PWM over time. Ixfx's [envelopes](https://clinth.github.io/ixfx-docs/modulation/envelope/) and [oscillators](https://clinth.github.io/ixfx-docs/modulation/oscillator/) would be useful
+* Experiment mixing colours between the three LEDs. ixfx's [colour module](https://clinth.github.io/ixfx-docs/types/colour/#parsing) can help you parse and interpolate colours to get red, green and blue values.
+
+## Read more
+
+* [Espruino Pulse width modulation](https://www.espruino.com/PWM)
+* [Espruino analogWrite](https://www.espruino.com/Reference#l__global_analogWrite) (for PWM)
+* [Espruino digitalWrite](https://www.espruino.com/Reference#l__global_digitalWrite) (for toggling LEDs)
---
+++ io/espruino/led-pwm-env/script.js
@@ -0,0 +1,118 @@
+import { continuously } from '../../../ixfx/flow.js';
+import { Espruino } from '../../../ixfx/io.js';
+import { adsr, defaultAdsrOpts } from '../../../ixfx/modulation.js';
+
+const settings = Object.freeze({
+  // Filter device list...
+  device: ``, // Put in the name of your device here, eg `Puck.js a123`
+  pwmLed:`LED2`,
+  env: adsr(defaultAdsrOpts())
+});
+
+let state = Object.freeze({
+  /** @type Espruino.EspruinoBleDevice|undefined */
+  puck:undefined,
+  /** @type number */
+  pwm: 0
+});
+
+/**
+ * Set LED pulse width modulation
+ * See: https://www.espruino.com/PWM
+ * and: https://www.espruino.com/Reference#l__global_analogWrite
+ * @param {string} pinName Eg 'LED1'
+ * @param {number} rate Pwm rate, 0..1
+ */
+const setLedPwm = (pinName, rate) => {
+  const { puck } = state;
+  if (!puck) throw new Error(`Puck not yet connected`);
+   
+  // Sanity check
+  if (rate < 0) throw new Error(`Rate must be at least 0 (${rate})`);
+  if (rate > 1) throw new Error(`Rate must no greater than 1 (${rate})`);
+  
+  let js = ``;
+
+  if (rate === 0) {
+    // Turn off
+    js = `digitalWrite(${pinName}, 0)\n`;
+  } else if (rate === 1) {
+    // Turn to full, skip PWM
+    js = `digitalWrite(${pinName}, 1)\n`;
+  } else {
+    // Pwm!
+    js = `analogWrite(${pinName}, ${rate})\n`;
+  }
+  console.log(js);
+  puck.write(js);
+};
+
+const loop = () => {
+  const { env } = settings;
+  const v = env.value;
+
+  if (!Number.isNaN(v)) {
+    updateState({ pwm: v });
+    console.log(v);  
+    setLedPwm(settings.pwmLed, state.pwm);
+  }
+  window.requestAnimationFrame(loop);
+};
+
+const setup = () => {
+  /**
+   * Set connected state
+   * @param {boolean} connected 
+   */
+  const onConnected = (connected) => {
+    setCssDisplay(`preamble`,  connected ? `none` : `block`);
+  };
+
+  const connect = async () => {
+    try {
+      // Filter by name, if defined in settings
+      const opts = settings.device.length > 0 ? { name: settings.device } : {};
+
+      // Connect to Puck
+      const p = await Espruino.puck(opts);
+      console.log(`Connected`);
+      
+      // Listen for events
+      p.addEventListener(`change`, evt => {
+        console.log(`${evt.priorState} -> ${evt.newState}`);
+      });
+
+      onConnected(true);
+      updateState({ puck: p });
+    } catch (ex) {
+      console.error(ex);
+      onConnected(false);
+    }
+  };
+
+  document.getElementById(`btnConnect`)?.addEventListener(`click`, connect);
+  
+  document.addEventListener(`keypress`, evt => {
+    settings.env.trigger();
+  });
+  loop();
+};
+setup();
+
+
+function setCssDisplay(id, value) {
+  const el = document.getElementById(id);
+  if (!el) return;
+  el.style.display = value;
+}
+
+/**
+ * Update state
+ * @param {Partial<state>} s 
+ */
+function updateState (s) {
+  state = Object.freeze({
+    ...state,
+    ...s
+  });
+}
