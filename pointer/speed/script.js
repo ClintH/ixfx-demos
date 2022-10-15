b5c7ab05166de8cdf06baefacdb2f907f32618d1
CH <clint@thestaticvoid.net>
2022-10-15 15:14:37 +0200

    pointer/speed simplify

--- pointer/speed/script.js
+++ pointer/speed/script.js
@@ -36,26 +36,65 @@ let state = Object.freeze({
 
   // Output of x,y movingAveragers
   /** @type {{x:number, y:number}} */
-  speedAvg: { x:0, y: 0 }
+  speedAvg: { x:0, y: 0 },
+  lastUpdate: Date.now()
 });
+
+const tick = () => {
+  const { avg } = settings;
+  const { movement, lastUpdate } = state;
+  const now = performance.now();
+
+  // Speed in x,y, made relative.
+  const speed = {
+    x: scale(movement.x / (now - lastUpdate)),
+    y: scale(movement.y / (now - lastUpdate))
+  };
+
+  updateState({
+    lastUpdate: now,
+    // Reset accumulated movement
+    movement: { x:0 , y: 0 },
+
+    // Update with latest calculated values
+    speed,
+    speedAvg: {
+      x: avg.x.add(speed.x),
+      y: avg.y.add(speed.y)
+    }
+  });
+};
 
 const useState = () => {
   const { speed, speedAvg } = state;
   const { fontWidth, fontWeight } = settings;
   
-  const lblSpeed = document.getElementById(`lblSpeed`);
-  const lblSpeedAvg = document.getElementById(`lblSpeedAvg`);
-  if (!lblSpeed || !lblSpeedAvg) return;
+  innerText(`debug`,
+    `
+    Speed: ${Points.toString(speed, 2)}
+    Speed average: ${Points.toString(speedAvg, 2)}
+  `);
+  
+  const width = Math.round(scalePercent(speedAvg.x, fontWidth[0], fontWidth[1]));
+  const weight = Math.round(scalePercent(speedAvg.y, fontWeight[0], fontWeight[1]));
+  setFontVariation(`speed`, width, weight);
 
-  lblSpeed.innerText = Points.toString(speed, 2);
-  lblSpeedAvg.innerText = Points.toString(speedAvg, 2);
+};
 
+/**
+ * Set the CSS font variation for an element by id
+ * @param {string} id 
+ * @param {number} width 
+ * @param {number} weight 
+ * @returns 
+ */
+const setFontVariation = (id, width, weight) => {
   const el = document.getElementById(`speed`);
   if (!el) return;
 
   // Generate CSS text for each variable font axis
-  const wdth = `'wdth' ` + Math.round(scalePercent(speedAvg.x, fontWidth[0], fontWidth[1]));
-  const wght = `'wght' ` + Math.round(scalePercent(speedAvg.y, fontWeight[0], fontWeight[1]));
+  const wdth = `'wdth' ` + width;
+  const wght = `'wght' ` + weight;
 
   // Apply to element
   // Note that axies must be in alphabetical order (!)
@@ -77,40 +116,16 @@ const onPointerMove = (evt) => {
     }
   });
 };
+
+// Scale & clamp speed with an input range of 0..maxSpeed. This yields a value of 0..1
+const scale = (v) => scaleClamped(v, 0, settings.maxSpeed);
+
 const setup = () => {
-  const { avg, maxSpeed } = settings;
-
   document.addEventListener(`pointermove`, onPointerMove);
-
-  // Scale & clamp speed with an input range of 0..maxSpeed. This yields a value of 0..1
-  const scale = (v) => scaleClamped(v, 0, maxSpeed);
-
-  let lastUpdate = performance.now();
 
   // Update speed every 50ms
   setInterval(() => {
-    const { movement } = state;
-    const now = performance.now();
-
-    // Speed in x,y, made relative.
-    const speed = {
-      x: scale(movement.x / (now - lastUpdate)),
-      y: scale(movement.y / (now - lastUpdate))
-    };
-
-    lastUpdate = now;
-
-    updateState({
-      // Reset accumulated movement
-      movement: { x:0 , y: 0 },
-
-      // Update with latest calculated values
-      speed,
-      speedAvg: {
-        x: avg.x.add(speed.x),
-        y: avg.y.add(speed.y)
-      }
-    });
+    tick();
     useState();
   }, settings.updateRateMs);
 };
@@ -127,3 +142,11 @@ function updateState (s) {
   });
 }
 
+function innerText(id, msg) {
+  const el = document.getElementById(id);
+  if (el) {
+    if (el.innerText !== msg) {
+      el.innerText = msg;
+    }
+  }
+}
