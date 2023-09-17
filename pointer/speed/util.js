9da73ce6274a4a3bc9a5a82995ee2583f5eb5def
CH <clint@thestaticvoid.net>
2023-09-17 22:23:40 +0200

    Update util.js

--- pointer/speed/util.js
+++ pointer/speed/util.js
@@ -9,3 +9,23 @@ export function textContent(id, message) {
     element.textContent = message;
   }
 }
+
+/**
+ * Set the CSS font variation for an element by id
+ * @param {string} id 
+ * @param {number} width 
+ * @param {number} weight 
+ * @returns 
+ */
+export const setFontVariation = (id, width, weight) => {
+  const element = /** @type HTMLElement */(document.querySelector(`#speed`));
+  if (!element) return;
+
+  // Generate CSS text for each variable font axis
+  const wdth = `'wdth' ` + width;
+  const wght = `'wght' ` + weight;
+
+  // Apply to element
+  // Note that axies must be in alphabetical order (!)
+  element.style.fontVariationSettings = `${wdth}, ${wght}`;
+};
