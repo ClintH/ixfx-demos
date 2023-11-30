49a2801720ffe7447813711a81f7fb4f45564f72
CH <clint@thestaticvoid.net>
2023-11-30 17:56:51 +0100

    espruino/led-pwm-env

--- io/espruino/led-pwm-env/script.js
+++ io/espruino/led-pwm-env/script.js
@@ -1,22 +1,30 @@
 import { continuously } from '../../../ixfx/flow.js';
 import { Espruino } from '../../../ixfx/io.js';
 import { adsr, defaultAdsrOpts as defaultAdsrOptions } from '../../../ixfx/modulation.js';
-import {setCssDisplay} from './util.js';
+import { setCssDisplay } from './util.js';
 
 const settings = Object.freeze({
   // Filter device list...
   device: ``, // Put in the name of your device here, eg `Puck.js a123`
-  pwmLed:`LED2`,
+  pwmLed: `LED2`,
   updateRateMs: 30,
+  // Use a simple ADSR envelope
   env: adsr(defaultAdsrOptions())
 });
 
+/**
+ * Type definition for state
+ * @typedef {Readonly<{
+ * puck: Espruino.EspruinoBleDevice|undefined
+ * pwm: number
+ * running: boolean
+ * }>} State
+ */
+
+/** @type State */
 let state = Object.freeze({
-  /** @type Espruino.EspruinoBleDevice|undefined */
-  puck:undefined,
-  /** @type number */
+  puck: undefined,
   pwm: 0,
-  /** @type boolean */
   running: false
 });
 
@@ -27,14 +35,14 @@ let state = Object.freeze({
  * @param {string} pinName Eg 'LED1'
  * @param {number} rate Pwm rate, 0..1
  */
-const setLedPwm = (pinName, rate) => {
+function setLedPwm(pinName, rate) {
   const { puck } = state;
   if (!puck) throw new Error(`Puck not yet connected`);
-   
+
   // Sanity check
   if (rate < 0) throw new Error(`Rate must be at least 0 (${rate})`);
   if (rate > 1) throw new Error(`Rate must no greater than 1 (${rate})`);
-  
+
   let js = ``;
 
   if (rate === 0) {
@@ -51,70 +59,105 @@ const setLedPwm = (pinName, rate) => {
   puck.write(js);
 };
 
-const update = () => {
-  const { env, updateRateMs } = settings;
+/**
+ * Use value from state
+ */
+function use() {
+  const { pwmLed } = settings;
+  const { pwm, running } = state;
+
+  if (running) {
+    // Running, set brightness
+    console.log(pwm);
+    setLedPwm(pwmLed, pwm);
+  } else {
+    // Not running, turn LED off
+    setLedPwm(pwmLed, 0);
+  }
+}
+
+/**
+ * Read value from envelope and save to state
+ */
+function update() {
+  const { env, updateRateMs, pwmLed } = settings;
+  let { running } = state;
+
+  // Get latest value from the envelope
   const v = env.value;
 
   if (Number.isNaN(v)) {
-    // Turn off
-    setLedPwm(settings.pwmLed, 0);
-    saveState({ running: false });
+    // Envelope has completed
+    running = false;
   } else {
-    saveState({ pwm: v, running: true });
-    console.log(v);  
-    setLedPwm(settings.pwmLed, state.pwm);
-  
-    // Queue to loop again
+    // Envelope is running, save value into state
+    running = true;
+  }
+  saveState({ pwm: v, running });
+
+  // Use current state
+  use();
+
+  // If the envelope has time remaining, queue 'update' to run again
+  if (running) {
     setTimeout(update, updateRateMs);
   }
 };
 
-function setup() {
-  /**
-   * Set connected state
-   * @param {boolean} connected 
-   */
-  const onConnected = (connected) => {
-    setCssDisplay(`preamble`,  connected ? `none` : `block`);
-  };
+/**
+ * On keypress event
+ * @param {KeyboardEvent} event 
+ */
+function onKeyPress(event) {
+  console.log(`Trigger`);
+  settings.env.trigger();
+  if (!state.running) update();
+}
 
-  const connect = async () => {
-    try {
-      // Filter by name, if defined in settings
-      const options = settings.device.length > 0 ? { name: settings.device } : {};
+async function connect() {
+  try {
+    // Filter by name, if defined in settings
+    const options = settings.device.length > 0 ? { name: settings.device } : {};
 
-      // Connect to Puck
-      const p = await Espruino.puck(options);
-      console.log(`Connected`);
-      
-      // Listen for events
-      p.addEventListener(`change`, event => {
-        console.log(`${event.priorState} -> ${event.newState}`);
-      });
+    // Connect to Puck
+    const p = await Espruino.puck(options);
+    console.log(`Connected`);
 
-      onConnected(true);
-      saveState({ puck: p });
-    } catch (error) {
-      console.error(error);
-      onConnected(false);
-    }
-  };
+    // Listen for events
+    p.addEventListener(`change`, event => {
+      console.log(`${event.priorState} -> ${event.newState}`);
+    });
 
-  document.querySelector(`#btnConnect`)?.addEventListener(`click`, connect);
-  
-  document.addEventListener(`keypress`, event => {
-    console.log(`Trigger`);
-    settings.env.trigger();
-    if (!state.running) update();
-  });
+    onConnected(true);
+    saveState({ puck: p });
+  } catch (error) {
+    console.error(error);
+    onConnected(false);
+  }
 };
+
+
+/**
+ * When connection state has changed
+ * @param {boolean} connected 
+ */
+function onConnected(connected) {
+  setCssDisplay(`preamble`, connected ? `none` : `block`);
+};
+
+function setup() {
+  document.querySelector(`#btnConnect`)?.addEventListener(`click`, connect);
+  document.addEventListener(`keypress`, onKeyPress);
+};
+
 setup();
+
 
 /**
  * Update state
  * @param {Partial<state>} s 
  */
-function saveState (s) {
+function saveState(s) {
   state = Object.freeze({
     ...state,
     ...s
