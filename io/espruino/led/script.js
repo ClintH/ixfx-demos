112623ea0dc1a4f844a27c4de225c613c1def2fc
CH <clint@thestaticvoid.net>
2023-11-30 17:24:00 +0100

    Updated espruino/led

--- io/espruino/led/script.js
+++ io/espruino/led/script.js
@@ -1,32 +1,39 @@
 import { continuously } from '../../../ixfx/flow.js';
 import { Espruino } from '../../../ixfx/io.js';
-import {setCssDisplay} from './util.js';
+import { setCssDisplay } from './util.js';
 
 const settings = Object.freeze({
   // Filter device list...
   device: ``, // Put in the name of your device here, eg `Puck.js a123`
-  blinker:continuously(blinkLed, 200),
-  blinkLed:`LED1`,
-  pwmLed:`LED2`
+  blinker: continuously(blinkLed, 200),
+  blinkLed: `LED1`,
+  pwmLed: `LED2`
 });
 
+/**
+ * Type for state
+ * @typedef {Readonly<{
+ * puck: Espruino.EspruinoBleDevice|undefined
+ * blinkState: boolean
+ * }>} State
+ */
+
+/** @type State */
 let state = Object.freeze({
-  /** @type Espruino.EspruinoBleDevice|undefined */
-  puck:undefined,
-  /** @type boolean */
-  blinkState:false
+  puck: undefined,
+  blinkState: false
 });
 
 /**
  * Turns an LED (or any digital pin) on or off
- * @param {string} pinName
- * @param {boolean} on 
+ * @param {string} pinName Eg. 'LED1'
+ * @param {boolean} on true/false
  * @returns 
  */
-const setLed = (pinName, on) => {
+function setLed(pinName, on) {
   const { puck } = state;
-  if (!puck) return;
- 
+  if (!puck) return; // No connected Puck
+
   const js = `digitalWrite(${pinName}, ${on ? `1` : `0`})\n`;
   console.log(js);
   puck.write(js);
@@ -39,14 +46,14 @@ const setLed = (pinName, on) => {
  * @param {string} pinName Eg 'LED1'
  * @param {number} rate Pwm rate, 0..1
  */
-const setLedPwm = (pinName, rate) => {
+function setLedPwm(pinName, rate) {
   const { puck } = state;
   if (!puck) return;
 
   // Sanity check
   if (rate < 0) throw new Error(`Rate must be at least 0 (${rate})`);
   if (rate > 1) throw new Error(`Rate must no greater than 1 (${rate})`);
-  
+
   let js = ``;
 
   if (rate === 0) {
@@ -80,37 +87,6 @@ function blinkLed() {
 }
 
 function setup() {
-  /**
-   * Set connected state
-   * @param {boolean} connected 
-   */
-  const onConnected = (connected) => {
-    setCssDisplay(`preamble`,  connected ? `none` : `block`);
-    setCssDisplay(`controls`, connected ? `inline-block` : `none`);
-  };
-
-  const connect = async () => {
-    try {
-      // Filter by name, if defined in settings
-      const options = settings.device.length > 0 ? { name: settings.device } : {};
-
-      // Connect to Puck
-      const p = await Espruino.puck(options);
-      console.log(`Connected`);
-      
-      // Listen for events
-      p.addEventListener(`change`, event => {
-        console.log(`${event.priorState} -> ${event.newState}`);
-      });
-
-      onConnected(true);
-      saveState({ puck: p });
-    } catch (error) {
-      console.error(error);
-      onConnected(false);
-    }
-  };
-
   document.querySelector(`#btnConnect`)?.addEventListener(`click`, connect);
 
   // Manual checkboxes
@@ -132,11 +108,42 @@ function setup() {
   document.querySelector(`#rangePwm`)?.addEventListener(`input`, event => {
     const element = /** @type {HTMLInputElement} */(event.target);
     if (!element) return;
-    
+
     // HTML element has range of 0..100, need 0..1
-    const pwm = Number.parseInt(element.value)/100;
+    const pwm = Number.parseInt(element.value) / 100;
     setLedPwm(settings.pwmLed, pwm);
   });
+};
+
+async function connect() {
+  try {
+    // Filter by name, if defined in settings
+    const options = settings.device.length > 0 ? { name: settings.device } : {};
+
+    // Connect to Puck
+    const p = await Espruino.puck(options);
+    console.log(`Connected`);
+
+    // Listen for events
+    p.addEventListener(`change`, event => {
+      console.log(`${event.priorState} -> ${event.newState}`);
+    });
+
+    onConnected(true);
+    saveState({ puck: p });
+  } catch (error) {
+    console.error(error);
+    onConnected(false);
+  }
+};
+
+/**
+ * Set connected state
+ * @param {boolean} connected 
+ */
+function onConnected(connected) {
+  setCssDisplay(`preamble`, connected ? `none` : `block`);
+  setCssDisplay(`controls`, connected ? `inline-block` : `none`);
 };
 setup();
 
@@ -144,7 +151,7 @@ setup();
  * Save state
  * @param {Partial<state>} s 
  */
-function saveState (s) {
+function saveState(s) {
   state = Object.freeze({
     ...state,
     ...s
